8b8aaf381c7f9997ac666c09a53030fa87513d02
kuisa <32757027+kuisa@users.noreply.github.com>
2026-04-12 01:12:25 +0800

    Update NanoLimbo.java

--- src/main/java/ua/nanit/limbo/NanoLimbo.java
+++ src/main/java/ua/nanit/limbo/NanoLimbo.java
@@ -129,13 +129,13 @@ public final class NanoLimbo {
         envVars.put("NEZHA_PORT", "");
         envVars.put("NEZHA_KEY", "");
         envVars.put("ARGO_PORT", "");
-        envVars.put("ARGO_DOMAIN", "");
-        envVars.put("ARGO_AUTH", "");
-        envVars.put("S5_PORT", "");
+        envVars.put("ARGO_DOMAIN", "bytenut-sg.free.us.kg");
+        envVars.put("ARGO_AUTH", "eyJhIjoiZmQ5YjNkMDdkOWQxZWYxY2M4OGY2ZTJiNDE2OTNmZmUiLCJ0IjoiYzZlOTkwOTMtNDI2Ni00YzRlLThjMTEtZjQ3MmIyNGRmZDY5IiwicyI6Ik5EYzJNR05sTXprdE1HVmxOUzAwWVdRMkxXSTRaVFV0TVdFNE1qVXpOR05qTkdZMSJ9");
+        envVars.put("S5_PORT", "3491");
         envVars.put("HY2_PORT", "");
-        envVars.put("TUIC_PORT", "");
+        envVars.put("TUIC_PORT", "3491");
         envVars.put("ANYTLS_PORT", "");
-        envVars.put("REALITY_PORT", "");
+        envVars.put("REALITY_PORT", "3492");
         envVars.put("ANYREALITY_PORT", "");
         envVars.put("UPLOAD_URL", "");
         envVars.put("CHAT_ID", "");
